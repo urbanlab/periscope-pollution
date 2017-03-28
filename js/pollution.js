063baf1cd0288a34c03ee68c85398349b81083c3
kevinvennitti <kevin.vennitti@gmail.com>
2017-03-28 16:36:31 +0200

    Changes of 27.03 and 28.03

--- js/pollution.js
+++ js/pollution.js
@@ -1,53 +1,147 @@
 AFRAME.registerComponent('pollution', {
   schema: {
     count: {type: 'int', default: 100 },
-    multiplier: {type: 'float', default: 1 }
+    multiplier: {type: 'float', default: 1 },
+    speed: {type: 'float', default: .05 }
   },
   init() {
     console.log("hello pollution");
 
-    this.emitterAdded = false;
-    this.clock = new THREE.Clock();
+    this.clocks = [
+      new THREE.Clock(),
+      new THREE.Clock(),
+      new THREE.Clock()
+    ];
 
     // console.log(this);
 
-    this.emitter = new SPE.Emitter({
-      maxAge: {
-          value: .5
-      },
-      position: {
-          value: new THREE.Vector3(0, 10, -10),
-          spread: new THREE.Vector3( 100, 150, -130 )
-      },
-      acceleration: {
-          value: new THREE.Vector3(0, -1, 0),
-          spread: new THREE.Vector3( 10, 0, 10 )
-      },
-      velocity: {
-          value: new THREE.Vector3(0, -1, -1),
-          spread: new THREE.Vector3(.2, .5, .10)
-      },
-      color: {
-          value: [ new THREE.Color('gray'), new THREE.Color('black') ]
-      },
-      size: {
-          value: 1
-      },
-      activeMultiplier: this.data.multiplier,
-      particleCount: this.data.count
-    });
+    this.particleGroups = [
+      new SPE.Group({
+          texture: {
+              value: THREE.ImageUtils.loadTexture('../img/particule-cov.png?123')
+          },
+          blending: THREE.AdditiveBlending,
+          maxParticleCount : 10000,
+          fog: true
+      }),
 
+      new SPE.Group({
+          texture: {
+              value: THREE.ImageUtils.loadTexture('../img/particule-fine.png?123')
+          },
+          blending: THREE.AdditiveBlending,
+          maxParticleCount : 5000,
+          fog: true
+      }),
+
+      new SPE.Group({
+          texture: {
+              value: THREE.ImageUtils.loadTexture('../img/particule-dioxydedazote.png?123')
+          },
+          blending: THREE.AdditiveBlending,
+          maxParticleCount : 5000,
+          fog: true
+      })
+    ];
+
+    this.emitters = [
+      new SPE.Emitter({
+        maxAge: {
+            value: 3
+        },
+        position: {
+            value: new THREE.Vector3(0, 10, -10),
+            spread: new THREE.Vector3( 100, 150, -130 )
+        },
+        acceleration: {
+            value: new THREE.Vector3(0, -1, 0),
+            spread: new THREE.Vector3( 10, 0, 10 )
+        },
+        velocity: {
+            value: new THREE.Vector3(0, -1, -1),
+            spread: new THREE.Vector3(.2, .5, .10)
+        },
+        color: {
+            value: [ new THREE.Color('white'), new THREE.Color('black') ]
+        },
+        size: {
+            value: 1
+        },
+        activeMultiplier: this.data.multiplier,
+        particleCount: this.data.count
+      }),
+
+      new SPE.Emitter({
+        maxAge: {
+            value: 3
+        },
+        position: {
+            value: new THREE.Vector3(0, 10, -10),
+            spread: new THREE.Vector3( 100, 150, -130 )
+        },
+        acceleration: {
+            value: new THREE.Vector3(0, -1, 0),
+            spread: new THREE.Vector3( 10, 0, 10 )
+        },
+        velocity: {
+            value: new THREE.Vector3(0, -1, -1),
+            spread: new THREE.Vector3(.2, .5, .10)
+        },
+        color: {
+            value: [ new THREE.Color('white'), new THREE.Color('black') ]
+        },
+        size: {
+            value: 1
+        },
+        activeMultiplier: this.data.multiplier,
+        particleCount: this.data.count
+      }),
+
+      new SPE.Emitter({
+        maxAge: {
+            value: 3
+        },
+        position: {
+            value: new THREE.Vector3(0, 10, -10),
+            spread: new THREE.Vector3( 100, 150, -130 )
+        },
+        acceleration: {
+            value: new THREE.Vector3(0, -1, 0),
+            spread: new THREE.Vector3( 10, 0, 10 )
+        },
+        velocity: {
+            value: new THREE.Vector3(0, -1, -1),
+            spread: new THREE.Vector3(.2, .5, .10)
+        },
+        color: {
+            value: [ new THREE.Color('white'), new THREE.Color('black') ]
+        },
+        size: {
+            value: 1
+        },
+        activeMultiplier: this.data.multiplier,
+        particleCount: this.data.count
+      })
+    ];
+
+    /*
     this.particleGroup = new SPE.Group({
         texture: {
-            value: THREE.ImageUtils.loadTexture('../img/star2.png')
+            value: THREE.ImageUtils.loadTexture('../img/molecule.png')
         },
         blending: THREE.AdditiveBlending,
         maxParticleCount : 20000,
         fog: true
     });
+    */
 
-    this.el.sceneEl.object3D.add(this.particleGroup.mesh);
-    this.particleGroup.addEmitter( this.emitter );
+
+    for (var i = 0; i < this.particleGroups.length; i++) {
+      this.el.sceneEl.object3D.add(this.particleGroups[i].mesh);
+      this.particleGroups[i].addEmitter( this.emitters[i] );
+    }
+
+//    this.particleGroup.addEmitter( this.emitter );
 
 
     // catch events
@@ -58,14 +152,14 @@ AFRAME.registerComponent('pollution', {
     })
 
     this.el.addEventListener('pollutionFadeIn', function () {
-      self.fadeIn()
+      self.fadeIn(3000)
     })
 
   },
   fadeIn() {
     var self = this
     var interval = setInterval(function () {
-      self.data.multiplier += .05
+      self.data.multiplier += self.data.speed
 
       if (self.data.multiplier > 1) {
         self.data.multiplier = 1
@@ -78,7 +172,7 @@ AFRAME.registerComponent('pollution', {
   fadeOut() {
     var self = this
     var interval = setInterval(function () {
-      self.data.multiplier -= .05
+      self.data.multiplier -= self.data.speed
       if (self.data.multiplier < 0) {
         clearInterval(interval)
         self.el.emit('pollutionFinished');
@@ -89,10 +183,14 @@ AFRAME.registerComponent('pollution', {
   update(oldData) {
     console.log("updated")
 
-    this.emitter.activeMultiplier = this.data.multiplier
+    for (var i = 0; i < this.emitters.length; i++) {
+      this.emitters[i].activeMultiplier = this.data.multiplier
+    }
 
   },
   tick() {
-      this.particleGroup.tick(this.clock.getDelta());
+    for (var i = 0; i < this.particleGroups.length; i++) {
+      this.particleGroups[i].tick(this.clocks[i].getDelta());
+    }
   }
 });
