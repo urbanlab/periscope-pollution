e18ea746ef4257c5e4643eb59e4f2dce8a18500d
Clément Renaud <clement.renaud@gmail.com>
2017-03-30 16:40:32 +0200

    start autoplay video on mobile

--- js/pollution.js
+++ js/pollution.js
@@ -20,129 +20,66 @@ AFRAME.registerComponent('pollution', {
           texture: {
               value: THREE.ImageUtils.loadTexture('./img/particule-cov.png?123')
           },
-          blending: THREE.AdditiveBlending,
-          maxParticleCount : 10000,
-          fog: true
+          // blending: THREE.AdditiveBlending,
+          maxParticleCount : 10000
+          // fog: true
       }),
 
       new SPE.Group({
           texture: {
               value: THREE.ImageUtils.loadTexture('./img/particule-fine.png?123')
           },
-          blending: THREE.AdditiveBlending,
-          maxParticleCount : 5000,
-          fog: true
+          // blending: THREE.AdditiveBlending,
+          maxParticleCount : 5000
+          // fog: true
       }),
 
       new SPE.Group({
           texture: {
               value: THREE.ImageUtils.loadTexture('./img/particule-dioxydedazote.png?123')
           },
-          blending: THREE.AdditiveBlending,
-          maxParticleCount : 5000,
-          fog: true
+          // blending: THREE.AdditiveBlending,
+          maxParticleCount : 5000
+          // fog: true
       })
     ];
+
+    var emitterSettings = {
+      maxAge: {
+          value: 3
+      },
+      position: {
+          value: new THREE.Vector3(0, 10, -10),
+          spread: new THREE.Vector3( 100, 150, -130 )
+      },
+      acceleration: {
+          value: new THREE.Vector3(0, -1, 0),
+          spread: new THREE.Vector3( 10, 0, 10 )
+      },
+      velocity: {
+          value: new THREE.Vector3(0, -1, -1),
+          spread: new THREE.Vector3(.2, .5, .10)
+      },
+      // color: {
+      //     value: [ new THREE.Color('white'), new THREE.Color("hsl(0, 10%, 10%)") ]
+      // },
+      size: {
+          value: 1
+      },
+      activeMultiplier: this.data.multiplier,
+      particleCount: this.data.count
+    }
 
     this.emitters = [
-      new SPE.Emitter({
-        maxAge: {
-            value: 3
-        },
-        position: {
-            value: new THREE.Vector3(0, 10, -10),
-            spread: new THREE.Vector3( 100, 150, -130 )
-        },
-        acceleration: {
-            value: new THREE.Vector3(0, -1, 0),
-            spread: new THREE.Vector3( 10, 0, 10 )
-        },
-        velocity: {
-            value: new THREE.Vector3(0, -1, -1),
-            spread: new THREE.Vector3(.2, .5, .10)
-        },
-        color: {
-            value: [ new THREE.Color('white'), new THREE.Color('black') ]
-        },
-        size: {
-            value: 1
-        },
-        activeMultiplier: this.data.multiplier,
-        particleCount: this.data.count
-      }),
-
-      new SPE.Emitter({
-        maxAge: {
-            value: 3
-        },
-        position: {
-            value: new THREE.Vector3(0, 10, -10),
-            spread: new THREE.Vector3( 100, 150, -130 )
-        },
-        acceleration: {
-            value: new THREE.Vector3(0, -1, 0),
-            spread: new THREE.Vector3( 10, 0, 10 )
-        },
-        velocity: {
-            value: new THREE.Vector3(0, -1, -1),
-            spread: new THREE.Vector3(.2, .5, .10)
-        },
-        color: {
-            value: [ new THREE.Color('white'), new THREE.Color('black') ]
-        },
-        size: {
-            value: 1
-        },
-        activeMultiplier: this.data.multiplier,
-        particleCount: this.data.count
-      }),
-
-      new SPE.Emitter({
-        maxAge: {
-            value: 3
-        },
-        position: {
-            value: new THREE.Vector3(0, 10, -10),
-            spread: new THREE.Vector3( 100, 150, -130 )
-        },
-        acceleration: {
-            value: new THREE.Vector3(0, -1, 0),
-            spread: new THREE.Vector3( 10, 0, 10 )
-        },
-        velocity: {
-            value: new THREE.Vector3(0, -1, -1),
-            spread: new THREE.Vector3(.2, .5, .10)
-        },
-        color: {
-            value: [ new THREE.Color('white'), new THREE.Color('black') ]
-        },
-        size: {
-            value: 1
-        },
-        activeMultiplier: this.data.multiplier,
-        particleCount: this.data.count
-      })
+      new SPE.Emitter(emitterSettings),
+      new SPE.Emitter(emitterSettings),
+      new SPE.Emitter(emitterSettings)
     ];
-
-    /*
-    this.particleGroup = new SPE.Group({
-        texture: {
-            value: THREE.ImageUtils.loadTexture('../img/molecule.png')
-        },
-        blending: THREE.AdditiveBlending,
-        maxParticleCount : 20000,
-        fog: true
-    });
-    */
-
 
     for (var i = 0; i < this.particleGroups.length; i++) {
       this.el.sceneEl.object3D.add(this.particleGroups[i].mesh);
       this.particleGroups[i].addEmitter( this.emitters[i] );
     }
-
-//    this.particleGroup.addEmitter( this.emitter );
-
 
     // catch events
     var self = this;
