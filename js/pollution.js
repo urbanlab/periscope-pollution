a78fb3bf9706a1a470dbacd0abe2b6cf5c64906a
Clément Renaud <clement.renaud@gmail.com>
2017-03-31 10:03:25 +0200

    back-n-forth workflow

--- js/pollution.js
+++ js/pollution.js
@@ -7,6 +7,9 @@ AFRAME.registerComponent('pollution', {
   init() {
     console.log("hello pollution");
 
+    console.log(this);
+
+
     this.clocks = [
       new THREE.Clock(),
       new THREE.Clock(),
@@ -21,7 +24,8 @@ AFRAME.registerComponent('pollution', {
               value: THREE.ImageUtils.loadTexture('./img/particule-cov.png?123')
           },
           // blending: THREE.AdditiveBlending,
-          maxParticleCount : 10000
+          maxParticleCount : 10000,
+          colorize :false
           // fog: true
       }),
 
@@ -30,7 +34,8 @@ AFRAME.registerComponent('pollution', {
               value: THREE.ImageUtils.loadTexture('./img/particule-fine.png?123')
           },
           // blending: THREE.AdditiveBlending,
-          maxParticleCount : 5000
+          colorize :false,
+          maxParticleCount : 10000
           // fog: true
       }),
 
@@ -39,7 +44,8 @@ AFRAME.registerComponent('pollution', {
               value: THREE.ImageUtils.loadTexture('./img/particule-dioxydedazote.png?123')
           },
           // blending: THREE.AdditiveBlending,
-          maxParticleCount : 5000
+          colorize :false,
+          maxParticleCount : 10000
           // fog: true
       })
     ];
@@ -60,9 +66,12 @@ AFRAME.registerComponent('pollution', {
           value: new THREE.Vector3(0, -1, -1),
           spread: new THREE.Vector3(.2, .5, .10)
       },
-      // color: {
-      //     value: [ new THREE.Color('white'), new THREE.Color("hsl(0, 10%, 10%)") ]
+      // opacity: {
+      //     value: [ 1, 1, 0 ]
       // },
+      color: {
+          value: [ new THREE.Color(0xFFFFFF), new THREE.Color(0x555555) ]
+      },
       size: {
           value: 1
       },
@@ -95,24 +104,27 @@ AFRAME.registerComponent('pollution', {
   },
   fadeIn() {
     var self = this
+    var sceneEl = document.getElementById("sceneRoom")
     var interval = setInterval(function () {
       self.data.multiplier += self.data.speed
-
-      if (self.data.multiplier > 1) {
+      console.log(self.data.multiplier);
+      if (self.data.multiplier >= 1) {
         self.data.multiplier = 1
         clearInterval(interval)
-        self.el.emit('pollutionReady');
+        console.log(self);
+        sceneEl.emit('pollutionReady');
       }
       self.update()
     }, 500);
   },
   fadeOut() {
     var self = this
+    var sceneEl = document.getElementById("sceneRoom")
     var interval = setInterval(function () {
       self.data.multiplier -= self.data.speed
-      if (self.data.multiplier < 0) {
+      if (self.data.multiplier < .01) {
         clearInterval(interval)
-        self.el.emit('pollutionFinished');
+        sceneEl.emit('pollutionFinished');
       }
       self.update()
     }, 500);
